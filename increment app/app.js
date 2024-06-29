c95a3443188d9622dc946aff38716749b9489614
Divakar5862 <152698080+Divakar5862@users.noreply.github.com>
2024-06-29 15:08:27 +0530

    Add files via upload

---
+++ increment app/app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const valueElement = document.getElementById("value");
+    const incrementButton = document.getElementById("increment");
+    const decrementButton = document.getElementById("decrement");
+
+    let counter = 0;
+
+    incrementButton.addEventListener("click", () => {
+        counter++;
+        valueElement.textContent = counter;
+    });
+
+    decrementButton.addEventListener("click", () => {
+        if (counter > 0) {
+            counter--;
+            valueElement.textContent = counter;
+        }
+    });
+});
